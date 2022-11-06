ac82482cf343c371e0972d4c79f511f23264f7be
Patel Hasan <Hasan.Patel@tetrapak.com>
2022-11-06 11:36:08 -0500

    New Coding Challenge, orderlyQueue.

---
+++ Orderly Queue/solution2.java
@@ -0,0 +1,17 @@
+class solution {
+	public String orderlyQueue(String s, int k) {
+		String ans = s;
+		for (int i = 0; i < s.length(); i++){
+			String temp = s.substring(i) + s.substring(0, i);
+			if (temp.compareTo(ans)) < 0 {
+				ans = temp;
+			}
+		}
+		return ans;
+	}else {
+		char[] chars = s.toCharArray();
+		Arrays.sort(chars);
+		return new String(chars);
+		}
+	}
+}
