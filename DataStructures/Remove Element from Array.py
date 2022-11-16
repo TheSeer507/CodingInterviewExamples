b0f5c394b4960217e9140c38d90acdf465e045b6
Patel Hasan <Hasan.Patel@tetrapak.com>
2022-11-15 21:27:09 -0500

    Added new Coding Challenge, Remove Element from Array

---
+++ DataStructures/Remove Element from Array.py
@@ -0,0 +1,22 @@
+#Given an integer array nums and an integer val,
+# remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+#Input: nums = [3,2,2,3], val = 3
+#Output: 2, nums = [2,2,_,_]
+#Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+
+#Approach use a pointer, k to shift the vallue to the right as long as it is not equal to vallue.
+
+class Solution:
+    def removeElement(self, nums: List[int], val:int) -> int:
+        #Initialize out pointer
+        k = 0
+        #Loop through the lenght of nums array
+        for i in range(len(nums)):
+        #if the number in the current position is not equal to val we replace our pointer with the current position of nums array
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        #We return our pointer which holds the number of element in the nums array
+        return k
+
+        
